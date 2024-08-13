ff5decba47df41c0e602a161bd2fb33467ba2f69
Sumit Meelu <sumit.meelu@ukg.com>
2024-08-13 13:54:26 +0530

    Exceptions added

---
+++ mentorService/src/main/java/com/ukg/lsm/utils/ErrorDetail.java
@@ -0,0 +1,16 @@
+package com.ukg.lsm.utils;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDetail {
+    private String code;
+    private String message;
+}
