006e429e1fd962a998ed73a00e344913d6f2bd17
ramansh <ramansh.goyal@ukg.com>
2024-08-16 13:42:18 +0530

    merge

---
+++ registry/src/main/java/com/ukg/LMS/RegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.ukg.LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegisteryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RegisteryApplication.class, args);
+	}
+}
