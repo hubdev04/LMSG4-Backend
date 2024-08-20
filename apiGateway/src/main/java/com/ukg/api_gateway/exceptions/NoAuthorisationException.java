641e7145d660ea27e689a3c224314dabe4d71755
Sujal Barsaiyan <sujal.barsaiyan1234@gmail.com>
2024-08-20 15:25:14 +0530

    pom updatr

--- apiGateway/src/main/java/com/ukg/api_gateway/exceptions/NoAuthorisationException.java
+++
@@ -1,7 +0,0 @@
-package com.ukg.api_gateway.exceptions;
-
-public class NoAuthorisationException extends Exception{
-    public NoAuthorisationException(String message) {
-        super(message);
-    }
-}
