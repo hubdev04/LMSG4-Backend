0d7959b6c3729be938408ef4546e8a4dd1b890d3
meelusumit <meelusumit.com>
2024-08-21 12:09:26 +0530

    made changes into corsConfig

--- apiGateway/src/main/java/com/lms/apiGateway/CorsConfig.java
+++ apiGateway/src/main/java/com/lms/apiGateway/CorsConfig.java
@@ -1,4 +1,5 @@
-package com.lms.apiGateway;
+package com.ukg.lsm.configuration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
